7e762c2bbfb4c113a77ed284a396ef67d72f87d0
Elijah Logan <elijah.logan@purchase.edu>
2019-11-24 08:43:26 -0500

    changed edax var ames

--- src/components/Pages/Edexes.js
+++ src/components/Pages/Edexes.js
@@ -1,11 +1,7 @@
-import React, {Fragment} from 'react'
-import {Link} from 'react-router-dom';
-import {Jumbotron, Button} from 'react-bootstrap';
-
-
-
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { Jumbotron, Button } from 'react-bootstrap';
 import styled from 'styled-components';
-
 
 //link will replace href
 
@@ -33,22 +29,56 @@ justify-content:center;
 align-items:center;
 `;
 
-
-const index_Whole = styled.section`
-
-
-
+//return that index's information
+const Index_Info = styled.section`
+ color: palevioletred;
+ font-size: 1em;
+ margin: 1em;
+ padding: 0.25em 1em;
+ border: 2px solid gray
+ border-radius: 3px;
+ width:50vw;
+ height:50vh
+ display:flex;
+ justify-content:center;
+ align-items:center;
 `;
 
 
 
 
-const Index_name = () => {
-    return(
-     <Index_Wrapper>
-d
-     </Index_Wrapper>
+
+
+function Index_Name() {
+  const [display, setDisplay] = useState(true);
+  const [index, setIndex] = useState(175);
+
+
+  const changeDisplay = () => {
+    let holder = display
+    holder = holder ? false : true
+    setDisplay(holder)
+
+  }
+  //return the index namee
+  if (display) {
+    return (
+      <Fragment>
+        <Index_Wrapper onClick={changeDisplay}>
+          {display}
+        </Index_Wrapper>
+      </Fragment>
     )
+  }
+  return (
+    <Index_Info onClick={changeDisplay}> hello </Index_Info>
+  )
+
+
+
+
 }
 
-export default Index_name
+
+
+export default Index_Name;
