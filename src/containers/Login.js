2b4d4d88762e7397e7021958e2ffdcb13415fa1f
Elijah Logan <elijah.logan@purchase.edu>
2019-12-27 23:07:31 -0500

    added better login for app

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Auth } from "aws-amplify";
 import { FormGroup, input } from "react-bootstrap";
 import "./Login.css";
-
+import { Fragment } from "react";
 export default function Login(props) {
     const [isLoading, setIsLoading] = useState(false);
     const [email, setEmail] = useState("john.doe@gmail.com")
@@ -26,28 +26,36 @@ export default function Login(props) {
     }
 
     return (
-        <div className="Login">
-            <form onSubmit={handleSubmit}>
-                <FormGroup controlId="email" bsSize="large">
-                    <label> email</label>
-                    <input
-                        autoFocus
-                        type="email"
-                        value={email}
-                        onChange={x => setEmail(x.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup controlId="password" bsSize="large">
-                    <label> password</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={x => setPassword(x.target.value)}
-                    />
-                </FormGroup>
-                <input type="submit" value="Submit" onClick={handleSubmit} />
+        <Fragment>
+            <h1>Login here</h1>
 
-            </form>
-        </div>
+
+
+            <div className="login" >
+
+                <div>
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" onChange={x => setEmail(x.target.value)} />
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" onChange={x => setPassword(x.target.value)} />
+                </div>
+
+                <div className="actions">
+                    <input type="submit" name="login" value="Login" onClick={handleSubmit} />
+                    <a href="/forgot">I forgot my password</a>
+                </div>
+            </div>
+
+
+            <div className="account">
+                <p>Create a new account <a href="">here</a>.</p>
+            </div>
+
+            <div className="contact">
+                <p>If you need any other help, please contact the helpdesk.</p>
+            </div>
+        </Fragment>
     );
 }
